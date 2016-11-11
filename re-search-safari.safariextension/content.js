2d00b037e55071978027bb29677ece32c31d7860
Sara Amani <Sara.Amani@semcon.com>
2016-11-11 16:16:04 +0100

    Window handling improvement.

--- re-search-safari.safariextension/content.js
+++ re-search-safari.safariextension/content.js
@@ -6,7 +6,6 @@
 	var inputSelector;
 	var titleTerm = false;
 	var englishTerms;
-	var windowName = false;
 
 	//Safari bug fix
 	//window.onunload = function(){};
@@ -19,15 +18,16 @@
 							action: "searchForTerm",
 							term: text,
 							windowWidth: window.outerWidth,
-							windowHeight: window.outerHeight
+							windowHeight: window.outerHeight,
+							windowScreenLeft: window.screenLeft,
+							windowScreenTop: window.screenTop
 					});
 			}
 	}
 
-	function resizeWindow(width, height){
-			console.log('IN RESIZE WINDOW FOR URL: ', window.location.href);
-			console.log('WIDTH TO RESIZE TO: ', width);
+	function resizeWindow(width, height, left, top){
 			window.resizeTo(width, height);
+			window.moveTo(left, top);
 	}
 
 	function getTitle() {
@@ -68,48 +68,48 @@
 
 	//Gets search terms when different events occur.
 	function getSearchTerm() {
-		console.log('SelectorInput: ', inputSelector);
-		elements = document.querySelectorAll(inputSelector);
-		if (elements.length === 0) {
-			setTimeout(getSearchTerm, 100);
-			console.log(inputSelector, '`s length was 0');
-			return false;
-		}
+			console.log('SelectorInput: ', inputSelector);
+			elements = document.querySelectorAll(inputSelector);
+			if (elements.length === 0) {
+					setTimeout(getSearchTerm, 100);
+					console.log(inputSelector, '`s length was 0');
+					return false;
+			}
 
-		var element = elements[0];
-		if (element.value.length > 0) {
-			console.log('if value is > 0');
-			sendText(element.value);
-		}
+			var element = elements[0];
+			if (element.value.length > 0) {
+					console.log('if value is > 0');
+					sendText(element.value);
+			}
 	}
 
 	function getSelectList(termsData) {
-		//Create and append select list
-		console.log(termsData);
-		var terms = Object.keys(termsData);
+			//Create and append select list
+			console.log(termsData);
+			var terms = Object.keys(termsData);
 
-		var selectList = document.createElement("SELECT");
-		selectList.setAttribute("style", "height: 25px; width: 164px; margin-top: 5px");
-		selectList.id = "termList";
+			var selectList = document.createElement("SELECT");
+			selectList.setAttribute("style", "height: 25px; width: 164px; margin-top: 5px");
+			selectList.id = "termList";
 
-		var defaultOption = document.createElement("option");
-		defaultOption.value = 'Other Re-search terms';
-		defaultOption.text = 'Other Re-search terms';
-		selectList.add(defaultOption);
+			var defaultOption = document.createElement("option");
+			defaultOption.value = 'Other Re-search terms';
+			defaultOption.text = 'Other Re-search terms';
+			selectList.add(defaultOption);
 
-		terms.sort(function(a, b) {
-			return a.localeCompare(b);
-		});
+			terms.sort(function(a, b) {
+					return a.localeCompare(b);
+			});
 
-		//Create and append the options
-		for (var i = 0; i < terms.length; i++) {
-			var option = document.createElement("option");
-			option.value = terms[i];
-			option.text = terms[i];
-			selectList.add(option);
-		}
+			//Create and append the options
+			for (var i = 0; i < terms.length; i++) {
+					var option = document.createElement("option");
+					option.value = terms[i];
+					option.text = terms[i];
+					selectList.add(option);
+			}
 
-		return selectList;
+			return selectList;
 	}
 
 	function checkElementMarginTop(element) {
@@ -120,7 +120,7 @@
 
 	function setEngineUI() {
 		if (inputSelector === '.gsfi') {
-			console.log('in Googles UI');
+			console.log('setting Googles UI');
 			var element = document.querySelectorAll('.sfbgg');
 			if (element.length > 0) {
 				element[0].setAttribute("style", "height: 90px; background-color: #f1f1f1; border-bottom: 1px solid #666; border-color: #e5e5e5; min-width: 980px;");
@@ -140,7 +140,8 @@
 		setEngineUI();
 		var selectList = getSelectList(englishTerms);
 		var elmnt = document.querySelectorAll('.tsf-p');
-		//Adapt Google UI
+
+		//Add select list to Googles UI
 		if (inputSelector === '.gsfi') {
 			if (elmnt.length > 0) {
 				elmnt[0].appendChild(selectList);
@@ -166,48 +167,27 @@
 		console.log(response);
 
 		if (response.message.hasOwnProperty("selectorSearchField")) {
-			console.log("Selectorsearchfield is here: ");
 			if (response.message.selectorSearchField !== false) {
-				console.log("inside selectorsearchfield if case");
 				console.log(response.message.selectorSearchField);
 				inputSelector = response.message.selectorSearchField;
 
 				if (runSetUI !== false) {
-					console.log("runSetUI method donepre");
 					englishTerms = response.message.englishTerms;
 					setUI();
 					runSetUI = false;
-					console.log("runSetUI method donepost");
 				}
 
 				titleTerm = document.getElementsByTagName('title')[0].innerText;
 				addListeners();
 				getSearchTerm();
-				console.log("get searchterm done");
 			} else {
 				console.log('Selector not found');
 			}
 		}
 
-		// else if(response.message.hasOwnProperty("runState")) {
-		// 		console.log(response);
-		// 		runState = response.message.runState;
-		// 		console.log("runstate is: ", runState);
-		// 	  console.log("runinit",runInit);
-		// 		if (runState === 'enabled' && runInit === true) {
-		// 				init();
-		// 				runInit = false;
-		// 				console.log("runInit is set to false")
-		// 		}
-		//
-		// 		else if (runState === 'disabled') {
-		// 				console.log('runState DISABLED');
-		// 		}
-		// }
-
 		else if(response.message.hasOwnProperty('resizeWindow')){
 				console.log('Should resize window');
-				resizeWindow(response.message.width, response.message.height);
+				resizeWindow(response.message.width, response.message.height, response.message.left, response.message.top);
 		}
 	}, false);
 
@@ -239,9 +219,6 @@
 				init();
 				runInit = false;
 		}
-		// safari.self.tab.dispatchMessage("getRunState", {
-		// 	action: 'getRunState'
-		// });
 	}
 
 
